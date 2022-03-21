d38484c95588c25ee43388c0ec825cefea95117c
DamnIgorNK <agadeem787@gmail.com>
2022-03-21 20:01:08 +0900

    crawl更新，複数サイトをクローリングしてからのパースできるようにした

--- scraping/src/chap4/crawl.py
+++ scraping/src/chap4/crawl.py
@@ -1,6 +1,7 @@
 
 import requests
 from bs4 import BeautifulSoup
+import re
 
 
 class Content:
@@ -18,6 +19,7 @@ class Content:
         print(f'TITLE: {self.title}')
         print(f'BODY: {self.body}')        
 
+
 class Website:
     """Webサイトの構造についての情報
 
@@ -26,12 +28,10 @@ class Website:
 
     """
 
-    def __init__(self, name, url, search_url, result_listing, result_url, absolute_url,title_tag, body_tag):
+    def __init__(self, name, url, target_pattern, absolute_url, title_tag, body_tag):
         self.name = name # サイト名
         self.url = url #サイトURL
-        self.search_url = search_url #対象のトピックを追加するときにどこで結果を探し始めるか
-        self.result_listing = result_listing #結果情報を格納する箱
-        self.result_url = result_url #結果を含むURL
+        self.target_pattern = target_pattern
         self.absolute_url = absolute_url # 絶対パス
         self.title_tag = title_tag # タイトルタグ，h1など
         self.body_tag = body_tag # 本文タグ，サイトによって異なる
@@ -39,6 +39,11 @@ class Website:
     
 class Crawler:
     """クローリングするクラス"""
+
+    def __init__(self, site):
+        self.site = site
+        self.visited = []
+
 
     def get_page(self,url:str):
         try:
@@ -48,9 +53,9 @@ class Crawler:
         return BeautifulSoup(req.text, 'html.parser')
 
     def safe_get(self, page_obj:BeautifulSoup, selector:str):
-        child_obj = page_obj.select(selector)
-        if child_obj is not None and len(child_obj)>0:
-            return child_obj[0].get_text()
+        selected_elems = page_obj.select(selector)
+        if selected_elems is not None and len(selected_elems)>0:
+            return selected_elems[0].get_text()
         return ""
 
     def search(self, topic, site):
@@ -80,35 +85,44 @@ class Crawler:
 
             
 
-def main():
-    crawler = Crawler()
-
-    site_data = {'name':'Reuters',
-                 'url':'http://reuters.com',
-                 'search_url':'http://www.reuters.com/search/news?blob=',
-                 'result_listing':'div.search-result-content',
-                 'result_url':'h3.search-result-title a',
-                 'absolute_url':False,
-                 'title_tag':'h1',
-                 'body_tag':'div.StandardArticleBody_body_1gnLA'} #bodyタグが間違っている可能性
+    def parse(self,url):
+        bs = self.get_page(url)
+        if bs is not None:
+            title = self.safe_get(bs, self.site.title_tag)
+            body = self.safe_get(bs, self.site.body_tag)
+            if title!='' and body!='':
+                content = Content(url, title, body)
+                content.printing()
 
     
-    sites = []
+    def crawl(self):
+        """Webサイトからホームページ取得"""
+        bs = self.get_page(self.site.url)
+        target_pages = bs.findAll('a', href=re.compile(self.site.target_pattern))
 
-    sites.append(Website(site_data['name'],
-                         site_data['url'],
-                         site_data['search_url'],
-                         site_data['result_listing'],
-                         site_data['result_url'],
-                         site_data['absolute_url'],
-                         site_data['title_tag'],
-                         site_data['body_tag']))
+        for target_page in target_pages:
+            target_page = target_page.attrs['href']
+            if target_page not in self.visited: # 既出かどうか
+                self.visited.append(target_page)
+                if not self.site.absolute_url:
+                    target_page = '{}{}'.format(self.site.url, target_page)
+                
+                #target_pageの解析
+                self.parse(target_page)
 
-    topics = ['python', 'data science']
-    for topic in topics:
-        print('GETTING INFO ABOUT ' + topic)
-        for target_site in sites:
-            crawler.search(topic, target_site)
+
+def main():
+    reuters = Website(
+        name='Reuters',
+        url='https://www.reuters.com',
+        target_pattern='^(/article/)',
+        absolute_url=False,
+        title_tag='h1',
+        body_tag='div.StandardArticleBody_body_1gnLA'
+    )
+
+    crawler = Crawler(reuters)
+    crawler.crawl()
 
 
 if __name__ == '__main__':
