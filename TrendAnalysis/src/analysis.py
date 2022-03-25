4a85d94b3d000242c4ef718c724db21292dd0232
DamnIgorNK <agadeem787@gmail.com>
2022-03-25 18:27:23 +0900

    Mecabの動作確認

--- TrendAnalysis/src/analysis.py
+++ TrendAnalysis/src/analysis.py
@@ -1,3 +1,22 @@
 # 拾ってきたツイートを元に感情分析を行う
 from mlask import MLAsk
-import boto3
+# 最初，MeCabは何故かImportErrorが出て太刀打ちできなかったが，以下のリンクを参考にMecabの動作を確認．
+# https://qiita.com/G1998G/items/2ad1b62c0285e478bfab
+
+
+class Feeling:
+    """MLAskで感情を分析するクラス"""
+
+    def __init__():
+        pass
+
+    def feeling(self, word):
+        """感情分析を行う関数，１０この感情をどう振り分けるかを検討中"""
+        analyzer = MLAsk()
+        ans = analyzer.analyze(word)
+
+
+
+
+
+
